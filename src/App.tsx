7ac02cc46ff3334ea9142e8692bf5a0c6478fcd5
吴海洋 <why@vip.local>
2023-03-01 15:08:16 +0800

    03

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
-/**
- * 1.useEffect是异步执行的
- * 2.useEffect中的函数会在每次组件渲染时都会被调用
- * 3.必须要注意不要让函数执行过于频繁，以避免影响性能(需要第二个参数解决)
+
+/** useEffect的第二个参数
+ * 1.参数是空数组, 那么useEffect仅在初始化执行
+ * 2.第二个参数（依赖项数组）来控制useEffect的触发时机
+ * 3.多个useEffect 依次执行
  */
+
 function App() {
   const [value, setValue] = useState<number>(0);
 
@@ -14,7 +16,11 @@ function App() {
   useEffect(() => {
     console.log("useEffect 被调用");
     document.title = `您有(${value})条新的消息`;
-  });
+  }, [value]); // 仅在 value 更改时更新
+
+  useEffect(() => {
+    console.log("空数组,仅在初始化执行一次");
+  }, []);
 
   console.log("组件渲染");
 
