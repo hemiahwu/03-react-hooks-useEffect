427de9f47bce159cd9dadb9b3bdc4bf9f65104db
吴海洋 <why@vip.local>
2023-03-01 14:45:11 +0800

    02

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,36 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
 /**
- * 1.在React中，组件的渲染是由状态（state）和属性（props）的变化触发的
- * 2.useEffect的目的是为了在组件渲染后执行副作用操作
- * 3.所谓副作用，是指与组件渲染无关的操作，比如网络请求、修改DOM、订阅事件等
+ * 1.useEffect是异步执行的
+ * 2.useEffect中的函数会在每次组件渲染时都会被调用
+ * 3.必须要注意不要让函数执行过于频繁，以避免影响性能(需要第二个参数解决)
  */
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [value, setValue] = useState<number>(0);
 
+  // 无关的状态
+  const [count, setCount] = useState<number>(0);
+
+  // 异步执行
   useEffect(() => {
-    console.log("useEffect的目的是为了在组件渲染后执行副作用操作");
+    console.log("useEffect 被调用");
+    document.title = `您有(${value})条新的消息`;
   });
 
+  console.log("组件渲染");
+
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        点我
+      </button>
+
+      {/* 无关的操作  */}
+      <h1>{count}</h1>
+      <button className="btn" onClick={() => setCount(count + 1)}>
+        增加数量
+      </button>
+    </>
   );
 }
 
